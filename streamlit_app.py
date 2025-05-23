bf9e5cd4af997589cfc941c7a5ad404d67c19350
SaskyaPutriAndina03 <adibasis80@gmail.com>
2025-05-23 16:48:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selamat datang, saya saskya")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Harus seratus pak ini effort ke warnet"
